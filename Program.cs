40314fbdefda430ce056760aaa3aa7e4c0c72848
Anna <107642838+Kashitsina@users.noreply.github.com>
2022-07-26 22:05:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿
+
+    void Zadacha57()
+{
+    // Составьте частотный словарь элементов двумерного массива
+    //частотный словарь содеожит информацию о том,сколько раз встречается элемент входныйх данных.
+    Random random = new Random();
+    int rows = random.Next(2, 6);
+    int columns = random.Next(3, 6);
+  
+ 
+    int[,] array = new int[rows, columns];
+    int[] dictionary = new int[10];
+    FillArray(array);
+    PrintArray(array);  
+    for (int i = 0; i < rows; i++)
+     {
+        for (int j = 0; j < columns; j++)
+        {
+           dictionary[array[i, j]] ++;
+        }
+     }
+    Console.WriteLine();
+    for (int i = 0; i < dictionary.Length; i++)
+    {
+        Console.WriteLine($"Элемент {i} встречается {dictionary[i]} раз");
+    }
+
+
+    
+ void FillArray(int[,] array)
+    {
+
+     for (int i = 0; i < rows; i++)
+     {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = random.Next(0 , 10);
+        }
+     }
+    }
+
+ void PrintArray(int[,] array)
+    {
+     Console.WriteLine();
+ 
+     for (int i = 0; i < rows; i++)
+        {
+           for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(array[i, j] + "\t");
+                }
+                Console.WriteLine();
+        }
+        Console.WriteLine();
+
+    }
+
+}   
+    //Zadacha57();
+    Zadacha57();
